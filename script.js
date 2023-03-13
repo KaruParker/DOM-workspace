f47e2f336b24c9af45b186563d5e07f79ceb3cc4
KaruParker <119045867+KaruParker@users.noreply.github.com>
2023-03-13 13:02:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// var ele=document.createElement("div");
+// ele.innerHTML="this is div";
+// ele.setAttribute=("class","main");
+// document.body.append(ele);
+
+
+// Assignment 1 :-
+// **************
+// var ele=document.createElement("div");
+// ele.innerHTML="My family";
+// ele.setAttribute("class","container");
+
+// var ele1=document.createElement("div");
+// ele1.innerHTML="5 members";
+// ele1.setAttribute("class","row")
+// ele.append(ele1);
+// document.body.append(ele);
+
+// var ele2=document.createElement("div");
+// ele2.innerHTML="Aachi Amma Appa Sister Brother";
+// ele2.setAttribute("class","col-1");
+// ele1.append(ele2);
+// document.body.append(ele);
+
+
+// submission task
+// ****************
+function foo(){
+var a1=document.getElementById("fname").value;
+console.log(a1);
+var a2=document.getElementById("mname").value;
+console.log(a2);
+var a3=document.getElementById("lname").value;
+console.log(a3);
+var a4=document.getElementById("email").value;
+console.log(a4);
+var a5=document.getElementById("pass").value;
+console.log(a5);
+}
